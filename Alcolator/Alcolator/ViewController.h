4d3e7c3c17b4a32b46828667d24d659f4c4d935b
Paulo Choi <makwanza@gmail.com>
2015-06-17 18:38:31 -0400

    Complete checkpoint 19 storyboarding alcolator

---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Paulo Choi on 6/17/15.
+//  Copyright (c) 2015 Paulo Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
