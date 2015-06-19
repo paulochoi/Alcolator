46e86fc6a048329c997207bd11640e904ea05a6a
Paulo Choi <makwanza@gmail.com>
2015-06-19 11:12:15 -0400

    Complete assignment 21, basic navigation

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Paulo Choi on 6/19/15.
+//  Copyright (c) 2015 Paulo Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
